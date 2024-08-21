c3ee34fd13e4055292f8d3cd3983663bb73ff552
Eduardo Decarli <decarliedu@gmail.com>
2024-08-21 13:23:05 -0300

    Iniciar o módulo open automaticamente

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-// Importar módulos necessários
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes');
 
-// Inicializar a aplicação Express
 const app = express();
 
 // Conectar ao banco de dados MongoDB
-mongoose.connect('mongodb://localhost:27017/pousada')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pousada')
     .then(() => {
         console.log('Conectado ao MongoDB');
     })
     .catch(err => {
         console.error('Erro ao conectar ao MongoDB:', err);
     });
+
 
 // Configurar middleware
 app.use(bodyParser.json());
@@ -32,8 +31,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// Middleware para tratamento de erros
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
+
 // Definir a porta e iniciar o servidor
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+    
+    // Importar o módulo open dinamicamente
+    const open = (await import('open')).default;
+    open(`http://localhost:${PORT}`);
 });
